2c99f17f9a2348fbf994b619baa72306b80de092
Pedro <pedro@localhost.localdomain>
2021-12-29 12:43:11 +0100

    Primera forma funcional del programa

--- getPorts.sh
+++ getPorts.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Illegal number of parameters, you only have to give the name of the grepeable file"
+	exit -1
+fi
+
 filename=$1
+
+if [ ! -f "$filename" ]; then
+	echo "$filename not exist."
+	exit -1
+fi
+
+
 info=$(cat $filename | grep 'Ports:' | tr '\t' '\n' | tr -d '()' | tr '/' ' ' | tr ':,' '\n')
 ports=$(echo "$info" | sed '1,3d' | awk '{print $1}' | tr '\n', ',' | tr -d ' ')
 ports=${ports::-1} 
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo cp getPorts.sh /usr/local/bin
+echo "alias getPorts='getPorts.sh'" >> .bashrc
+
